8e711e749b93ff418400cf2ebfd81b2da548d66c
papiyadas-git <112078918+papiyadas-git@users.noreply.github.com>
2022-08-28 00:40:35 +0300

    Add files via upload

---
+++ Exercise 2.3.py
@@ -0,0 +1,6 @@
+L = float(input("enter the length of rectangle: "))
+W = float(input("enter the width of rectangle: "))
+perimeter_of_the_rectangle = 2*(L+W)
+area_of_the_rectangle = L * W
+print("perimeter of the rectangle is", perimeter_of_the_rectangle)
+print("area of the rectangle is", area_of_the_rectangle)

77d55dc4135025f3ff1e78ba28f050c04a46000c
papiyadas-git <112078918+papiyadas-git@users.noreply.github.com>
2022-08-28 01:22:08 +0300

    Add files via upload

---
+++ Exercise 2.4.py
@@ -0,0 +1,9 @@
+a = float(input("enter first integer: "))
+b = float(input("enter second integer: "))
+c = float(input("enter third integer: "))
+sum = a+b+c
+product = a*b*c
+average = sum / 3
+print("sum of three integer is:", sum)
+print("product of three integer is:", product)
+print("average of three integer is:", average)

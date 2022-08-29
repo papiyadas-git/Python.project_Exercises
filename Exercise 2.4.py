350485b86531f43fd2187a89624ec73f0c7316ef
papiya <papiya.das@metropolia.fi>
2022-08-29 15:57:58 +0300

    testing

---
+++ Exercise 2.4.py
@@ -0,0 +1,10 @@
+num1 = float(input("enter first integer: "))
+num2 = float(input("enter second integer: "))
+num3 = float(input("enter third integer: "))
+sum = num1 + num2 + num3
+product = num1 * num2 * num3
+average = sum / 3
+print("sum of three integer is:", sum)
+print("product of three integer is:", product)
+print("average of three integer is:", average)
+
